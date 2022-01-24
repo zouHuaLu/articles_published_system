a61da952ce0cffd58e33633a97fd33b919670066
zouHuaLu <huangjie1234huang@qq.com>
2022-01-24 18:49:51 +0800

    文章列表页

--- src/components/Articles/Article.jsx
+++ src/components/Articles/Article.jsx
@@ -1,18 +1,104 @@
 import { observer } from "mobx-react-lite";
-import { Button } from "antd";
 import { useNavigate } from "react-router-dom";
+import { Table, Tag, Space, Button, Anchor } from "antd";
 
 export const Article = observer(() => {
+  const { Link } = Anchor;
   const naviagte = useNavigate();
   const toWriteArticle = () => {
     naviagte("/writeArticle");
   };
+  const columns = [
+    {
+      title: "标题",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "日期",
+      dataIndex: "age",
+      key: "age",
+    },
+    {
+      title: "分类",
+      dataIndex: "address",
+      key: "address",
+    },
+    {
+      title: "标签",
+      key: "tags",
+      dataIndex: "tags",
+      render: (tags) => (
+        <>
+          {tags.map((tag) => {
+            let color = tag.length > 5 ? "geekblue" : "green";
+            if (tag === "loser") {
+              color = "volcano";
+            }
+            return (
+              <Tag color={color} key={tag}>
+                {tag.toUpperCase()}
+              </Tag>
+            );
+          })}
+        </>
+      ),
+    },
+    {
+      title: "URL",
+      key: "action",
+      render: (text, record) => (
+        <Anchor>
+          <Link href={record.name} title={record.name} />
+        </Anchor>
+      ),
+    },
+    {
+      title: "操作",
+      key: "action",
+      render: (text, record) => (
+        <Space size="middle">
+          <Button type="primary">编辑</Button>
+          <Button type="primary" danger>
+            删除
+          </Button>
+        </Space>
+      ),
+    },
+  ];
+
+  const data = [
+    {
+      key: "1",
+      name: "John Brown",
+      age: 32,
+      address: "New York No. 1 Lake Park",
+      tags: ["nice", "developer"],
+    },
+    {
+      key: "2",
+      name: "Jim Green",
+      age: 42,
+      address: "London No. 1 Lake Park",
+      tags: ["loser"],
+    },
+    {
+      key: "3",
+      name: "Joe Black",
+      age: 32,
+      address: "Sidney No. 1 Lake Park",
+      tags: ["cool", "teacher"],
+    },
+  ];
+
   return (
-    <div>
-      这是文章页
-      <Button type="primary" onClick={toWriteArticle}>
-        写文章
-      </Button>
-    </div>
+    <>
+      <div>
+        <Button type="primary" onClick={toWriteArticle}>
+          写文章
+        </Button>
+      </div>
+      <Table columns={columns} dataSource={data} />
+    </>
   );
 });
